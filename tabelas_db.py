05e79f865f1483f2779247e8c04301f0dd65a45e
Alice <alicelourencoreis@gmail.com>
2024-10-25 12:30:27 -0300

    banco de pacientes funcionando faltando os links

--- tabelas_db.py
+++ tabelas_db.py
@@ -3,7 +3,13 @@ import sqlite3
 banco = sqlite3.connect('pacientes.db')
 cursor = banco.cursor()
 
-#cursor.execute("CREATE TABLE IF NOT EXISTS pacientes (id, nome, diagnostico, idade, sexo)")
-
-
+'''
+CREATE TABLE pacientes (
+    id INTEGER PRIMARY KEY,
+    nome TEXT NOT NULL,
+    diagnostico TEXT NOT NULL,
+    idade INTEGER,
+    sexo TEXT NOT NULL
+    );
+'''
 
